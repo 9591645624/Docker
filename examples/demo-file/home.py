90000a397ea3bf4e67ec7bd76681286276b88c0b
Prajwal S R <109466896+9591645624@users.noreply.github.com>
2026-02-17 12:15:30 +0530

    Add files via upload

---
+++ examples/demo-file/home.py
@@ -0,0 +1,10 @@
+# The Driving licence eligible or not check
+age = 20
+age1 = 25
+
+if age > age1:
+    print("Not eligible for driving licence")
+elif age1 > age:
+    print("Eligible for driving licence")
+else:
+    print("Both ages are not  equal")
---
+++ examples/python-web-page/hello.py
@@ -0,0 +1,2 @@
+Name  = "Prajwal"
+print (Name);
